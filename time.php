aa10edfa5671a625b5fc991e41df22d76fb1e3f2
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 17:29:46 -0400

    Added abs() call to timehash to avoid negatives.

--- time.php
+++ time.php
@@ -10,9 +10,9 @@ class time
         $now3 = date("is");
         $now4 = date("H");
         $micro = str_replace(" ", "", number_format((microtime() + 11644477200) * 10000000, 0, '.', ''));
-        $datetime = (((($now1 + $now2)* 100.0) * (($now3 . $now4) * 100.0) * $micro)*100.0) ;
+        $datetime = (((($now1 + $now2) * 100.0) * (($now3 . $now4) * 100.0) * $micro) * 100.0);
         
-        return str_split(trim(str_replace(".", "", str_replace(",", "", number_format(trim($datetime, "00") / rand(), 5))), "00"), strlen($length));
+        return str_split(abs(trim(str_replace(".", "", str_replace(",", "", number_format(trim($datetime, "00") / rand(), 5))), "00")), strlen($length));
         // no support for retreiving the era in php
     }
 }
