09539c16cae199e139127dcdfd06cc82ccdecfcd
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 18:47:02 -0400

    Added more unique/uniform time hashing.

--- time.php
+++ time.php
@@ -7,12 +7,13 @@ class time
     {
         $now1 = date("Z");
         $now2 = date("Y");
-        $now3 = date("is");
+        $now3 = date("i");
         $now4 = date("H");
+        $now5 = date("s");
         $micro = str_replace(" ", "", number_format((microtime() + 11644477200) * 10000000, 0, '.', ''));
-        $datetime = (((($now1 + $now2) * 100.0) * (($now3 . $now4) * 100.0) * $micro) * 100.0);
+        $datetime = ((($now1 + $now2) * 100.0) . ($now3 + $now4 + $now5) . $micro);
         
-        return str_split(abs(trim(str_replace(".", "", str_replace(",", "", number_format(trim($datetime, "00") / rand(), 5))), "00")), strlen($length));
+        return str_split(trim(str_replace(".", "", str_replace(",", "", number_format(abs(trim($datetime, "00") / rand()), 23))), "00"), strlen($length));
         // no support for retreiving the era in php
     }
 }
