fc6a2e65fdec44a1e2c2aee9704d8bb00c66a239
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 19:11:17 -0400

    Uniform time hash.

--- time.php
+++ time.php
@@ -11,9 +11,9 @@ class time
         $now4 = date("H");
         $now5 = date("s");
         $micro = str_replace(" ", "", number_format((microtime() + 11644477200) * 10000000, 0, '.', ''));
-        $datetime = ((($now1 + $now2) * 100.0) . ($now3 + $now4 + $now5) . $micro);
+        $datetime = (($now1 + $now2) . ($now3 + $now4 + $now5) . $micro) * 100.0;
         
-        return str_split(trim(str_replace(".", "", str_replace(",", "", number_format(abs(trim($datetime, "00") / rand()), 23))), "00"), strlen($length));
+        return str_split(trim(str_replace(".", "", str_replace(",", "", number_format(round(abs($datetime / rand(1.000000,9.999999))), 23))), "00"), strlen($length));
         // no support for retreiving the era in php
     }
 }
