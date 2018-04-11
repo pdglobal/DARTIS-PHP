d298277d9c0ae1201a34a00ae382e0c25f1994c0
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 23:28:12 -0400

    Added a modifier paramater.
    Passing an incrementing value to the crypt function, will then pass the
    value to the timestamp function, ensuring you don't get the same
    timestamp twice.

--- time.php
+++ time.php
@@ -3,15 +3,13 @@
 class time
 {
 
-    public function getTimestamp($length)
+    public function getTimestamp($length, $count)
     {
         $micro = str_replace(" ", "", number_format((microtime() + 11644477200) * 10000000, 0, '.', ''));
-        $min = 0;
-        $max = 99999999;
         if (substr($micro, 0, 1) == "-") {
-            $micro = strrev(abs($micro));  //I know this isn't anywhere near an optimal solution, looking for any suggestions on a better way to handle this
+            $micro = strrev(abs($micro)); // I know this isn't anywhere near an optimal solution, looking for any suggestions on a better way to handle this
         }
-        return str_split(str_replace(",", "", str_replace(".", "", number_format($micro + (mt_rand($min * 10, $max * 10) / 10)))), strlen($length));
+        return str_split(str_replace(",", "", str_replace(".", "", number_format($micro + $count))), strlen($length));
     }
 }
 ?>
