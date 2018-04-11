0770edfff5e7d679ec418d0736eed6293c5942a6
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 20:21:59 -0400

    Uniform timehash.

--- time.php
+++ time.php
@@ -13,8 +13,8 @@ class time
         $micro = str_replace(" ", "", number_format((microtime() + 11644477200) * 10000000, 0, '.', ''));
         $datetime = (($now1 + $now2) . ($now3 + $now4 + $now5) . $micro) * 100.0;
         
-        return str_split(trim(str_replace(".", "", str_replace(",", "", number_format(round(abs($datetime / rand(1.000000,9.999999))), 23))), "00"), strlen($length));
-        // no support for retreiving the era in php
+        return str_split(substr(trim(str_replace(".", "", str_replace(",", "", number_format(round(abs($datetime / rand(1.000000,9.999999))), 23))), "00"),1,23), strlen($length));
+        // no support for dealing with numbers aproaching infinity in php
     }
 }
 ?>
