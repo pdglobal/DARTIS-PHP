870baef5749dd2b834fcc5e00297d7ceeec282e0
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 21:59:02 -0400

    Added handler for negative time(before unix epoch)

--- time.php
+++ time.php
@@ -8,6 +8,9 @@ class time
         $micro = str_replace(" ", "", number_format((microtime() + 11644477200) * 10000000, 0, '.', ''));
         $min = 0;
         $max = 99999999;
+        if (substr($micro, 0 , 1) == "-") {
+            $micro = strrev(abs($micro));
+        }
         return str_split(str_replace(",", "", str_replace(".", "", number_format($micro+(mt_rand ($min*10, $max*10) / 10)))), strlen($length));
     }
 }
