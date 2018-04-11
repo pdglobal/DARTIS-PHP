bf50800d0f68a15f9a7c22a436401b7772774b98
Administrator <Administrator@CLOUD.intranet.pdglobal.net>
2018-04-10 22:07:33 -0400

    Formatting

--- time.php
+++ time.php
@@ -2,16 +2,16 @@
 
 class time
 {
-    
+
     public function getTimestamp($length)
     {
         $micro = str_replace(" ", "", number_format((microtime() + 11644477200) * 10000000, 0, '.', ''));
         $min = 0;
         $max = 99999999;
-        if (substr($micro, 0 , 1) == "-") {
+        if (substr($micro, 0, 1) == "-") {
             $micro = strrev(abs($micro));
         }
-        return str_split(str_replace(",", "", str_replace(".", "", number_format($micro+(mt_rand ($min*10, $max*10) / 10)))), strlen($length));
+        return str_split(str_replace(",", "", str_replace(".", "", number_format($micro + (mt_rand($min * 10, $max * 10) / 10)))), strlen($length));
     }
 }
 ?>
