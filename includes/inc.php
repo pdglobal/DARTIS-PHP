5ee4bada0822e063618d65af6fe5b2fade9e56a9
Zachary Litzinger <zach@pdglobal.app>
2020-01-11 08:45:23 -0500

    Added inc.php to includes dir

---
+++ includes/inc.php
@@ -0,0 +1,11 @@
+<?PHP
+namespace pdglobal\dartis;
+include_once 'keys.php';
+include_once 'construct.php';
+include_once 'time.php';
+include_once 'strings.php';
+include_once 'math.php';
+include_once 'crypt.php';
+class inc {
+}
+?>
